e61441a88db38c4dfb94aff7db17a0b805631743
Seras3 <adrian_etto@yahoo.com>
2022-01-30 11:27:20 +0200

    F***ING docker

--- frontend/src/pages/TvMovementPage.js
+++ frontend/src/pages/TvMovementPage.js
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
 import { Box } from '@mui/material';
 
 import PageTemplate from '../components/PageTemplate';
 
 const MovementPage = () => {
+  let data = {};
+
+  const fetchData = async () => {
+    try {
+      data = await fetch('http://lamp:8080/status');
+    } catch (e) {
+      console.log({ error: e });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log({ data });
+
   return (
     <PageTemplate>
       <Box></Box>
