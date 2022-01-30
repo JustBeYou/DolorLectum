c615a9aea8dd5c6197f052f57ab8a84324aea4ac
Radu Onea <radu.onea@ibm.com>
2022-01-30 18:30:53 +0200

    tests

---
+++ thermalsystem/openapi_server/tests/test_state.py
@@ -0,0 +1,24 @@
+import os
+import tempfile
+
+import pytest
+
+from ..__main__ import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+    yield app
+
+@pytest.fixture
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
+
+
+@pytest.fixture
+def runner(app):
+    """A test runner for the app's Click commands."""
+    return app.test_cli_runner()
+
